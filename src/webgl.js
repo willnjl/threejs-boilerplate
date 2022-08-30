01fec16a66cf46d003f23395c713d0c829291415
Will Leighton <will@fifteenten.co.uk>
2022-08-30 13:58:27 +0100

    update to es6

--- src/webgl.js
+++ src/webgl.js
@@ -1,62 +1,60 @@
-var WEBGL = {
+export const WEBGL = {
   isWebGLAvailable: function () {
     try {
-      var canvas = document.createElement('canvas')
+      const canvas = document.createElement('canvas');
       return !!(
         window.WebGLRenderingContext &&
         (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'))
-      )
+      );
     } catch (e) {
-      return false
+      return false;
     }
   },
 
   isWebGL2Available: function () {
     try {
-      var canvas = document.createElement('canvas')
-      return !!(window.WebGL2RenderingContext && canvas.getContext('webgl2'))
+      const canvas = document.createElement('canvas');
+      return !!(window.WebGL2RenderingContext && canvas.getContext('webgl2'));
     } catch (e) {
-      return false
+      return false;
     }
   },
 
   getWebGLErrorMessage: function () {
-    return this.getErrorMessage(1)
+    return this.getErrorMessage(1);
   },
 
   getWebGL2ErrorMessage: function () {
-    return this.getErrorMessage(2)
+    return this.getErrorMessage(2);
   },
 
   getErrorMessage: function (version) {
-    var names = {
+    const names = {
       1: 'WebGL',
       2: 'WebGL 2',
-    }
+    };
 
-    var contexts = {
+    const contexts = {
       1: window.WebGLRenderingContext,
       2: window.WebGL2RenderingContext,
-    }
+    };
 
-    var message =
-      'Your $0 does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">$1</a>'
+    const message =
+      'Your $0 does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">$1</a>';
 
-    var element = document.createElement('div')
-    element.id = 'webgl-error-message'
+    const element = document.createElement('div');
+    element.id = 'webgl-error-message';
 
     if (contexts[version]) {
-      message = message.replace('$0', 'graphics card')
+      message = message.replace('$0', 'graphics card');
     } else {
-      message = message.replace('$0', 'browser')
+      message = message.replace('$0', 'browser');
     }
 
-    message = message.replace('$1', names[version])
+    message = message.replace('$1', names[version]);
 
-    element.innerHTML = message
+    element.innerHTML = message;
 
-    return element
+    return element;
   },
-}
-
-module.exports = { WEBGL }
+};
