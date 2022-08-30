fdc3efc22315685c9274a1d1cb6c5f6edff352f7
Will Leighton <will@fifteenten.co.uk>
2022-08-30 13:11:47 +0100

    add lighting and emmisive light

--- src/index.js
+++ src/index.js
@@ -41,14 +41,17 @@ if (WEBGL.isWebGLAvailable()) {
   animate();
 
   const createShape = () => {
-    let radius = 15;
-    let height = 15;
+    let radius = 5;
+    let height = 5;
     let segments = 32;
     let color = 0xddddff;
-
+    let emissive = 0x9999cc; // color unneffected by other lighting
     // an object consists of three things.
     const geometry = new THREE.ConeGeometry(radius, height, segments); // geometry of the shape
-    const material = new THREE.MeshPhongMaterial({ color }); //what is the shape made out of and how does it reflect light
+    const material = new THREE.MeshLambertMaterial({
+      color,
+      emissive,
+    }); //what is the shape made out of and how does it reflect light
     const shape = new THREE.Mesh(geometry, material); //A mesh is a combination of the geometry and material.
 
     shape.rotateX(0.5);
@@ -58,6 +61,9 @@ if (WEBGL.isWebGLAvailable()) {
   };
 
   createShape();
+
+  const light = new THREE.DirectionalLight(0xffffff, 0.5);
+  scene.add(light);
 } else {
   var warning = WEBGL.getWebGLErrorMessage();
   document.body.appendChild(warning);
